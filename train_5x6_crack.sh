0e7b4c74a6edcc7ebc579627054583e0f9bbc05f
tcs1.joysort.net <bill@joysort.com>
2022-08-31 00:36:10 +0800

    add x6 training file

---
+++ train_5x6_crack.sh
@@ -0,0 +1,11 @@
+python train.py \
+  --weights 'yolov5x6.pt' \
+  --data 'dataset/crack/data.yaml' \
+  --hyp 'data/hyps/obb/hyp.finetune_dota.yaml' \
+  --epochs 10000 \
+  --batch-size 32 \
+  --img 224 \
+  --device 0,1 \
+  --patience 300 \
+  --name 'hazelnut_crack_224_x6_run'
+#  870cc68f3848eaaee11287ef56061f89ea8774df
